30cb2016ecc78a48180163d67d3fd8562b35cb14
Junjie Luo <junjie.luo.jhu@gmail.com>
2023-11-05 11:45:21 -0500

    update

--- rec2feat/rft_dataset.py
+++ rec2feat/rft_dataset.py
@@ -5,31 +5,24 @@ from torch.utils.data import Dataset
 import pandas as pd
 from functools import reduce
 from .rft_fn import get_df_RecDB_of_PDTInfo, process_CONFIG_CkpdRecNameFlt_of_PDTInfo
+from .rft_fn import get_df_TknDB_of_PDTInfo, process_CONFIG_TknGrn_of_PDTInfoCRF
+from .rft_fn import process_CONFIG_CMP_of_PDTInfoCRFT
+from .utils_vocab import build_SynFldVocab, get_update_SynFldVocab_and_GrnSeqName
 
+CACHE_NUM = 10
 
-CACHE_NUM = 100
-
-
-def collate_fn_for_df_PDT_CRF(batch_input):
+def collate_fn_for_df_PDT(batch_input):
     ##############
     # inputs: you can check the following inputs in the above cells.
     # (1): relational_list
     # (2): new_full_recfldgrn
     # (3): suffix
     ##############
-    df_PDT = pd.concat([i for i in batch_input]).reset_index(drop = True)
-    
+    df_PDT = pd.DataFrame([i for i in batch_input]).reset_index(drop = True)
     return df_PDT
 
 
-
-
-
 class CkpdRecFltDataset(Dataset):
-    
-    # df_RecDB: defined by Patient and RecName
-    df_RecDB_Store = {}
-    # df_TknDB_Store = {}
     
     def __init__(self, df_PDT_all, CONFIG_PDT, CONFIG_CkpdRecNameFlt, 
                  UTILS_Flt = None, RANGE_SIZE = None, CACHE_NUM = CACHE_NUM):
@@ -39,6 +32,20 @@ class CkpdRecFltDataset(Dataset):
         self.UTILS_Flt = UTILS_Flt
         self.RANGE_SIZE = RANGE_SIZE
         self.CACHE_NUM = CACHE_NUM
+        self.CkpdRecFlt = self.get_CkpdRecFlt_Name()
+        
+        # this might not be shared across different CkpdRecFltDataset instances.
+        self.df_RecDB_Store = {}
+        
+
+    def get_CkpdRecFlt_Name(self):
+        CkpdName = self.CONFIG_CkpdRecNameFlt['CONFIG_Ckpd']['CkpdName'] # 'Bf1D'
+        RecName =  self.CONFIG_CkpdRecNameFlt['CONFIG_RecDB']['RecName']
+        FilterName = self.CONFIG_CkpdRecNameFlt['CONFIG_Flt']['FilterName'] # 'Whl'
+        CkpdRec = CkpdName + '.' + RecName
+        CkpdRecFlt = CkpdRec + '.' + FilterName
+        return CkpdRecFlt
+    
         
     def __len__(self):
         return len(self.df_PDT_all)
@@ -61,8 +68,139 @@ class CkpdRecFltDataset(Dataset):
                                                                       self.df_RecDB_Store, self.UTILS_Flt)
         
         # (3) adjust size of df_RecDB
-        if len(df_RecDB) >= self.CACHE_NUM:
-            df_RecDB = df_RecDB.sample(int(self.CACHE_NUM / 6 * 5))
+        Group_List = list(df_RecDB['Group'].unique())
+        if len(Group_List) >= self.CACHE_NUM:
+            first_Group = df_RecDB.iloc[0]['Group']
+            last_Group = df_RecDB.iloc[-1]['Group']
+            Groups_to_keep = [i for i in Group_List if i not in [first_Group, last_Group]][1:] + [last_Group]
+            df_RecDB = df_RecDB[df_RecDB['Group'].isin(Groups_to_keep)].reset_index(drop = True)
+            # df_RecDB = df_RecDB.sample(int(self.CACHE_NUM / 6 * 5))
             self.df_RecDB_Store[RecName] = df_RecDB
             
         return PDTInfo_CkpdRecFlt
+    
+
+
+class CkpdRecFltTknDataset(CkpdRecFltDataset):
+    
+    def __init__(self, CRFDataset, CONFIG_TknDB, UNK_TOKEN):
+        self.CRFDataset = CRFDataset
+        self.CONFIG_PDT = CRFDataset.CONFIG_PDT
+        self.CONFIG_CkpdRecNameFlt = CRFDataset.CONFIG_CkpdRecNameFlt
+        self.CONFIG_TknDB = CONFIG_TknDB
+        
+        self.RANGE_SIZE = CRFDataset.RANGE_SIZE
+        self.CACHE_NUM = CRFDataset.CACHE_NUM
+        self.UNK_TOKEN = UNK_TOKEN # TODO: update to special token list in the future
+        
+        self.SynFldVocab = self.get_SynFldVocab()
+        self.SynFldGrn, self.CkpdRecFltTkn = self.get_CkpdRecFltTkn_Name()
+        
+        # this might not be shared across different CkpdRecFltDataset instances.
+        self.df_TknDB_Store = {}
+        
+    def get_CkpdRecFltTkn_Name(self):
+        CkpdRecFlt = self.CRFDataset.CkpdRecFlt
+        Ckpd, RecName, FilterName = CkpdRecFlt.split('.')
+        GrnName = self.CONFIG_TknDB['GrnName']
+        SynFldGrn = f'Rof{RecName}{FilterName}-{GrnName}'
+        CkpdRecFltTkn = CkpdRecFlt + '.' + SynFldGrn
+        return SynFldGrn, CkpdRecFltTkn
+        
+    def get_SynFldVocab(self):
+        fldgrnv_folder = self.CONFIG_TknDB['fldgrnv_folder']
+        RecFldGrn_List = self.CONFIG_TknDB['RecFldGrn_List'] 
+        RDTCal_rfg = self.CONFIG_TknDB['RDTCal_rfg'] 
+        SynFldVocab = build_SynFldVocab(fldgrnv_folder, RecFldGrn_List, RDTCal_rfg, self.UNK_TOKEN) 
+        return SynFldVocab
+        
+    def __len__(self):
+        return len(self.CRFDataset)
+    
+    def __getitem__(self, index):
+        
+        PDTInfo_CRF = self.CRFDataset[index]
+        
+        # (1) intake and update self.df_TknDB_Store.
+        SynFldGrn, df_TknDB, self.df_TknDB_Store = get_df_TknDB_of_PDTInfo(PDTInfo_CRF, 
+                                                                          self.CONFIG_PDT, 
+                                                                          self.CONFIG_TknDB, 
+                                                                          self.df_TknDB_Store, 
+                                                                          self.RANGE_SIZE)
+
+
+        # (2) load PDTInfo_CkpdRecFlt
+        
+        PDTInfo_CRFT = process_CONFIG_TknGrn_of_PDTInfoCRF(PDTInfo_CRF, 
+                                                           self.CONFIG_PDT, 
+                                                           self.CONFIG_TknDB, 
+                                                           self.df_TknDB_Store)
+    
+        # (3) adjust size of df_RecDB
+        Group_List = list(df_TknDB['Group'].unique())
+        if len(Group_List) >= self.CACHE_NUM:
+            first_Group = df_TknDB.iloc[0]['Group']
+            last_Group = df_TknDB.iloc[-1]['Group']
+            Groups_to_keep = [i for i in Group_List if i not in [first_Group, last_Group]][1:] + [last_Group]
+            df_TknDB = df_TknDB[df_TknDB['Group'].isin(Groups_to_keep)].reset_index(drop = True)
+            # df_RecDB = df_RecDB.sample(int(self.CACHE_NUM / 6 * 5))
+            self.df_TknDB_Store[SynFldGrn] = df_TknDB
+            
+        return PDTInfo_CRFT
+
+
+
+
+class CkpdRecFltTknCmpDataset(Dataset):
+    
+    def __init__(self, CRFTDataset, CONFIG_CMP, UTILS_CMP, UNK_TOKEN):
+        self.CRFTDataset = CRFTDataset
+        self.CONFIG_PDT = CRFTDataset.CONFIG_PDT
+        self.CONFIG_CkpdRecNameFlt = CRFTDataset.CONFIG_CkpdRecNameFlt
+        self.CONFIG_TknDB = CRFTDataset.CONFIG_TknDB
+        self.CONFIG_CMP = CONFIG_CMP
+        
+        self.UTILS_CMP = UTILS_CMP
+        
+        self.RANGE_SIZE = CRFTDataset.RANGE_SIZE
+        self.CACHE_NUM = CRFTDataset.CACHE_NUM
+        self.UNK_TOKEN = UNK_TOKEN # TODO: update to special token list in the future
+        
+        self.SynFldVocab = self.get_SynFldVocab()
+        self.SynFldVocabNew, self.CkpdRecFltGrnCmp, self.CkpdRecFltGrnCmpFeat = self.get_SynFldVocabNew_and_FeatNames()
+        
+    def get_SynFldVocab(self):
+        fldgrnv_folder = self.CONFIG_TknDB['fldgrnv_folder']
+        RecFldGrn_List = self.CONFIG_TknDB['RecFldGrn_List'] 
+        RDTCal_rfg = self.CONFIG_TknDB['RDTCal_rfg'] 
+        SynFldVocab = build_SynFldVocab(fldgrnv_folder, RecFldGrn_List, RDTCal_rfg, self.UNK_TOKEN) 
+        return SynFldVocab
+    
+    def get_SynFldVocabNew_and_FeatNames(self):
+        PDTName = self.CONFIG_PDT['PDTName']
+        
+        CkpdName, RecName, FilterName, SynFldGrn = self.CRFTDataset.CkpdRecFltTkn.split('.')
+        
+        SynFldVocab = self.SynFldVocab
+        CompressArgs = self.CONFIG_CMP['CompressArgs']
+        prefix_layer_cols = self.CONFIG_CMP['prefix_layer_cols']
+        focal_layer_cols = self.CONFIG_CMP['focal_layer_cols']
+        
+        SynFldVocabNew, CkpdRecFltGrnCmp, CkpdRecFltGrnCmpFeat = \
+            get_update_SynFldVocab_and_GrnSeqName(PDTName, CkpdName, RecName, FilterName, SynFldGrn, 
+                                                  SynFldVocab, CompressArgs, prefix_layer_cols, focal_layer_cols)
+        
+        return SynFldVocabNew, CkpdRecFltGrnCmp, CkpdRecFltGrnCmpFeat
+
+    def __len__(self):
+        return len(self.CRFTDataset)
+    
+    def __getitem__(self, index):
+        
+        PDTInfo_CRFT = self.CRFTDataset[index]
+        PDTInfo_CRFTC = process_CONFIG_CMP_of_PDTInfoCRFT(
+                                      PDTInfo_CRFT, self.CONFIG_PDT, self.CONFIG_TknDB, self.CONFIG_CMP, 
+                                      self.SynFldVocabNew, self.CkpdRecFltGrnCmp, self.CkpdRecFltGrnCmpFeat, 
+                                      UTILS_CMP = self.UTILS_CMP, UNK_TOKEN = self.UNK_TOKEN)
+            
+        return PDTInfo_CRFTC
--- rec2feat/rft_fn.py
+++ rec2feat/rft_fn.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from recfldgrn.datapoint import convert_PID_to_PIDgroup
-from .utils_ckpdrec import get_info_from_settings, get_Ckpd_from_PredDT, get_CkpdName_RecTable
-from .utils_recflt import filter_CkpdRec_fn
+from .utils_ckpdrec import get_group_info, get_Ckpd_from_PredDT, get_CkpdName_RecTable
+from .utils_recflt import filter_CkpdRec_fn, add_RelativeDT_to_dfCkpdRec
 from .utils_grnseq import get_df_ckpdrecfltgrn, generate_dfempty
 from .utils_vocab import build_SynFldVocab, get_update_SynFldVocab_and_GrnSeqName
 from .utils_cmp import compress_dfrec_with_CompressArgs, convert_df_compressed_to_DPLevel_tensor
-from .utils_fn import UTILS_Flt, UTILS_CMP
+from .utils_tool import UTILS_Flt, UTILS_CMP
 
 IDNAME = 'PID'
 RANGE_SIZE = 100
@@ -39,14 +39,19 @@ def get_df_RecDB_of_PDTInfo(PDTInfo, CONFIG_PDT, CONFIG_CkpdRecNameFlt, df_RecDB
     }
     assert RecNameTpt.split('_')[0] == RecName
     
-    df_RecDB_v1 = df_RecDB_Store.get(RecName, pd.DataFrame(columns = ['PID']))
-    if PDTInfo['PID'] not in df_RecDB_v1['PID'].to_list() : 
-        df_RecDB_v2 = get_info_from_settings(Group, None, RecChain_ARGS, RecInfo_ARGS)
+    df_RecDB_v1 = df_RecDB_Store.get(RecName, pd.DataFrame(columns = ['PID', 'Group']))
+    
+    # check whether the group information is store in the df_RecBD_Store.
+    # Group = convert_PID_to_PIDgroup(PDTInfo['PID'], RANGE_SIZE)
+    if Group not in df_RecDB_v1['Group'].unique() : 
+        df_RecDB_v2 = get_group_info(Group, RecChain_ARGS, RecInfo_ARGS)
         df_RecDB = pd.concat([df_RecDB_v1, df_RecDB_v2]).reset_index(drop = True)
     else:
         df_RecDB = df_RecDB_v1  
         
     df_RecDB_Store[RecName] = df_RecDB
+    # attention: PID might not be in df_RecDB, 
+    #            it is because PID doesn't have any records to contribute to that Group.
     return RecName, df_RecDB, df_RecDB_Store
 
 
@@ -76,27 +81,129 @@ def process_CONFIG_CkpdRecNameFlt_of_PDTInfo(PDTInfo, CONFIG_PDT, CONFIG_CkpdRec
     RecNameTpt =  CONFIG_RecDB['RecNameTpt']
     df_RecDB = df_RecDB_Store[RecName]
     CkpdRec = CkpdName + '.' + RecName
+    # P_RecDB could be empty.
     P_RecDB = df_RecDB[df_RecDB['PID'] == PDTInfo['PID']].iloc[0]
     PDTInfo[CkpdRec] = get_CkpdName_RecTable(PDTInfo, CkpdName, P_RecDB, RecNameTpt)
     
     # -------- 3. CkpdName.RecName.Filter
     FilterName = CONFIG_Flt['FilterName'] # 'Whl'
     CkpdRecFlt = CkpdRec + '.' + FilterName
-    if FilterName != 'Whl':
-        FilterName2FilterFunc = UTILS_Flt['FilterName2FilterFunc']
-        FilterName2Share = UTILS_Flt['FilterName2Share']
-        RecName2ThresConfig = UTILS_Flt['RecName2ThresConfig']
-        CkpdRecFilter_ARGS = {
-            'FilterName': FilterName,
-            'FilterFunc': FilterName2FilterFunc[FilterName],
-            'ThresShare': FilterName2Share[FilterName],
-            'ThresConfig': RecName2ThresConfig[RecNameTpt.split('_')[0]]
-        }
-        # df_PDT[CkpdRecFlt] = df_PDT.apply(lambda x: filter_CkpdRec_fn(x['PredDT'], x[CkpdRec], **CkpdRecFilter_ARGS), axis = 1)
-        PDTInfo[CkpdRecFlt] = filter_CkpdRec_fn(PDTInfo['PredDT'], PDTInfo[CkpdRec], **CkpdRecFilter_ARGS) # PDTInfo[CkpdRec]
-    else:
-        # df_PDT[CkpdRecFlt] = df_PDT[CkpdRec]
-        PDTInfo[CkpdRecFlt] = PDTInfo[CkpdRec]
-        
+    
+    # if FilterName != 'Whl':
+    FilterName2FilterFunc = UTILS_Flt['FilterName2FilterFunc']
+    FilterName2Share = UTILS_Flt['FilterName2Share']
+    RecName2ThresConfig = UTILS_Flt['RecName2ThresConfig']
+    CkpdRecFilter_ARGS = {
+        'FilterName': FilterName,
+        'FilterFunc': FilterName2FilterFunc[FilterName],
+        'ThresShare': FilterName2Share[FilterName],
+        'ThresConfig': RecName2ThresConfig[RecNameTpt.split('_')[0]]
+    }
+    # df_PDT[CkpdRecFlt] = df_PDT.apply(lambda x: filter_CkpdRec_fn(x['PredDT'], x[CkpdRec], **CkpdRecFilter_ARGS), axis = 1)
+    PDTInfo[CkpdRecFlt] = filter_CkpdRec_fn(PDTInfo['PredDT'], PDTInfo[CkpdRec], **CkpdRecFilter_ARGS) # PDTInfo[CkpdRec]
+    
     PDTInfo_CkpdRecFlt = PDTInfo
     return PDTInfo_CkpdRecFlt
+
+
+def get_df_TknDB_of_PDTInfo(PDTInfo_CRF, CONFIG_PDT, CONFIG_TknDB, df_TknDB_Store, RANGE_SIZE):
+
+    PDTInfo = PDTInfo_CRF.copy()
+    CkpdRecFlt = PDTInfo.keys()[-1]
+    Ckpd, RecName, FilterName = CkpdRecFlt.split('.')
+    pdt_folder = CONFIG_PDT['pdt_folder']
+    PDTName = CONFIG_PDT['PDTName']
+    Group = convert_PID_to_PIDgroup(PDTInfo['PID'], RANGE_SIZE)
+    
+    # load variables
+    fldgrn_folder = CONFIG_TknDB['fldgrn_folder']
+    GrnName = CONFIG_TknDB['GrnName']
+    RecFldGrn_List = CONFIG_TknDB['RecFldGrn_List'] 
+    SynFldGrn = f'Rof{RecName}{FilterName}-{GrnName}'
+    GrnSeqChain_ARGS = {'PID': {'folder': pdt_folder, 'RecName': PDTName}}
+    GrnSeqInfo_ARGS = {fld:  {'folder': fldgrn_folder,  'RecName': fld,  'Columns': 'ALL'} for fld in RecFldGrn_List}
+    
+    # check whether the group information is store in the df_RecBD_Store.
+    # Group = convert_PID_to_PIDgroup(PDTInfo['PID'], RANGE_SIZE)
+    df_TknDB_v1 = df_TknDB_Store.get(SynFldGrn, pd.DataFrame(columns = ['PID', 'Group']))
+    if Group not in df_TknDB_v1['Group'].unique() : 
+        df_TknDB_v2 = get_group_info(Group, GrnSeqChain_ARGS, GrnSeqInfo_ARGS)
+        df_TknDB = pd.concat([df_TknDB_v1, df_TknDB_v2]).reset_index(drop = True)
+    else:
+        df_TknDB = df_TknDB_v1  
+        
+    df_TknDB_Store[SynFldGrn] = df_TknDB
+    return SynFldGrn, df_TknDB, df_TknDB_Store
+
+
+def process_CONFIG_TknGrn_of_PDTInfoCRF(PDTInfo_CRF, CONFIG_PDT, CONFIG_TknDB, df_TknDB_Store):
+    PDTInfo = PDTInfo_CRF.copy()
+
+    # CkpdRecFlt = df_PDT.columns[-1]
+    CkpdRecFlt = PDTInfo.keys()[-1]
+    
+    Ckpd, RecName, FilterName = CkpdRecFlt.split('.')
+    pdt_folder = CONFIG_PDT['pdt_folder']
+    PDTName = CONFIG_PDT['PDTName']
+    
+    # -------- CONFIG_GrnSeq
+    fldgrn_folder = CONFIG_TknDB['fldgrn_folder']
+    fldgrnv_folder = CONFIG_TknDB['fldgrnv_folder']
+    GrnName = CONFIG_TknDB['GrnName']
+    RecFldGrn_List = CONFIG_TknDB['RecFldGrn_List'] 
+    RDTCal_rfg = CONFIG_TknDB['RDTCal_rfg'] 
+    SynFldGrn = f'Rof{RecName}{FilterName}-{GrnName}'
+    CkpdRecFltGrn = CkpdRecFlt + '.' + SynFldGrn
+    
+    # get df ckpdrecfltgrn
+    if type(PDTInfo[CkpdRecFlt]) == type(pd.DataFrame()):
+        df_TknDB = df_TknDB_Store[SynFldGrn]
+        P_TknDB = df_TknDB[df_TknDB['PID'] == PDTInfo['PID']].iloc[0]
+        PDTInfo[CkpdRecFltGrn] = get_df_ckpdrecfltgrn(PDTInfo[CkpdRecFlt], P_TknDB, RecFldGrn_List, RDTCal_rfg, RecName, SynFldGrn)
+    else:
+        PDTInfo[CkpdRecFltGrn] = generate_dfempty(PDTInfo['PID'], PDTInfo['PredDT'], CkpdRecFltGrn, UNK_TOKEN)
+    
+    PDTInfo_CkpdRecFltTkn = PDTInfo
+    # SynFldVocab = build_SynFldVocab(fldgrnv_folder, RecFldGrn_List, RDTCal_rfg, UNK_TOKEN)  
+    return PDTInfo_CkpdRecFltTkn # , CkpdRecFltTkn, SynFldVocab
+
+
+def process_CONFIG_CMP_of_PDTInfoCRFT(PDTInfo_CRFT, CONFIG_PDT, CONFIG_TknDB, CONFIG_CMP, 
+                                      SynFldVocabNew, CkpdRecFltGrnCmp, CkpdRecFltGrnCmpFeat, 
+                                      UTILS_CMP = UTILS_CMP, UNK_TOKEN = UNK_TOKEN):
+    # df_PDT = df_PDT_CkpdRecFltTkn.copy()
+    PDTInfo = PDTInfo_CRFT.copy()
+    
+    # CkpdRecFlt = df_PDT.columns[-2]
+    CkpdRecFlt = PDTInfo.keys()[-2]
+    # CkpdRecFltGrn =  df_PDT.columns[-1]
+    CkpdRecFltGrn =  PDTInfo.keys()[-1]
+    CkpdName, RecName, FilterName = CkpdRecFlt.split('.')
+
+    pdt_folder = CONFIG_PDT['pdt_folder']
+    PDTName = CONFIG_PDT['PDTName']
+    
+    # -------- CONFIG_CMP
+    fldgrn_folder = CONFIG_TknDB['fldgrn_folder']
+    fldgrnv_folder = CONFIG_TknDB['fldgrnv_folder']
+    GrnName = CONFIG_TknDB['GrnName']
+    RecFldGrn_List = CONFIG_TknDB['RecFldGrn_List']  
+    RDTCal_rfg = CONFIG_TknDB['RDTCal_rfg'] 
+    SynFldGrn = f'Rof{RecName}{FilterName}-{GrnName}'
+    
+    
+    method_to_fn = UTILS_CMP['method_to_fn']
+    CompressArgs = CONFIG_CMP['CompressArgs']
+    prefix_layer_cols = CONFIG_CMP['prefix_layer_cols']
+    focal_layer_cols = CONFIG_CMP['focal_layer_cols']
+    
+
+    PDTInfo[CkpdRecFltGrnCmp] = compress_dfrec_with_CompressArgs(PDTInfo[CkpdRecFltGrn], CompressArgs, SynFldGrn, 
+                                                                 CkpdRecFltGrnCmp, SynFldVocabNew, method_to_fn)
+    
+    PDTInfo[CkpdRecFltGrnCmpFeat] = convert_df_compressed_to_DPLevel_tensor(PDTInfo[CkpdRecFltGrnCmp], SynFldVocabNew, 
+                                                                            CkpdRecFltGrnCmp, CkpdRecFltGrnCmpFeat, 
+                                                                            prefix_layer_cols, focal_layer_cols)
+    
+    PDTInfo_CkpdRecFltTknCmpFeat = PDTInfo
+    return PDTInfo_CkpdRecFltTknCmpFeat # , SynFldVocab, CkpdRecFltGrnCmp, CkpdRecFltGrnCmpFeat
