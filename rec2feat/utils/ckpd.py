a5b35de046b37f318003f4f3565b0221b65ab44e
Junjie Luo <junjie.luo.jhu@gmail.com>
2023-11-10 01:35:57 -0500

    update ckpd fn

--- rec2feat/utils/ckpd.py
+++ rec2feat/utils/ckpd.py
@@ -4,7 +4,7 @@ def get_Ckpd_from_PredDT(PredDT, DistStartToPredDT, DistEndToPredDT, TimeUnit, *
     assert TimeUnit in ['H', 'D', 'min']
     DT_s = PredDT + pd.to_timedelta(DistStartToPredDT, unit = TimeUnit)
     DT_e = PredDT + pd.to_timedelta(DistEndToPredDT, unit = TimeUnit)
-    return {'DT_s': DT_s, 'DT_e':DT_e}
+    return pd.DataFrame([{'DT_s': DT_s, 'DT_e':DT_e}])
 
 def process_CONFIG_Ckpd_of_PDTInfo(Case, CONFIG_Ckpd):
     PDTInfo = Case.copy()
